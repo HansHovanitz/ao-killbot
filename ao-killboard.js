5cc7ac051491e4326e4450c0df9c6bb4943365be
HansHovanitz <sellerhans@gmail.com>
2017-09-21 18:27:22 -0700

    remove express

--- ao-killboard.js
+++ ao-killboard.js
@@ -5,24 +5,6 @@
 * @Last Modified time: 2017-08-21 12:35:33
 */
 
-
-// lazy fix for Heroku
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
-
-
-
 // Define static constants
 const config = require('./config.json');
 
